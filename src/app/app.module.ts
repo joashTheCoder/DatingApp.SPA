9986b5bf24f43e81f5997b31e24b122b25e84376
Joash Llaneza <joash.llaneza7@gmail.com>
2018-07-25 00:30:24 +0800

    Injecting angular services to components but has staticinjectorerror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { HttpModule } from '@angular/http';
+// import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpModule
+      HttpModule,
+      FormsModule,
+      // HttpClientModule
    ],
-   providers: [],
+   providers: [
+       AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
